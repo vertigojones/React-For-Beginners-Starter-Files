f78b9fe975c92fec8c1196456e73c0f50d62f3f1
owenliversidge <owen.liversidge@gmail.com>
2018-03-07 22:26:03 -0500

    got a bit further

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -17,6 +17,8 @@ class AddFishForm extends Component {
       image: this.imageRef.value.value
     };
     this.props.addFish(fish)
+    // refresh the form
+    event.currentTarget.reset()
   };
 
   render() {
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,27 +1,60 @@
-import React, { Component } from 'react'
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
+import React, { Component } from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
-    state = {
-        fishes: {},
-        order: {}
-    }
-    addFish = (fish) => {
-
-    }
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market"/>
-                </div>
-                    <Order />
-                    <Inventory addFish={this.addFish}/>
-            </div>
-        )
-    }
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+  addToOrder = key => {
+    // take a copy of state
+    const order = { ...this.state.order };
+    // add money to order
+    order[key] = order.fish + 1 || 1;
+    // call setstate to update
+    this.setState({ order });
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
